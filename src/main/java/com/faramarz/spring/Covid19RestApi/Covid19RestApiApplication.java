ff2221d5123c1fe3375787e06c9cd5cd73b73e82
FaramarzAf <faramarzafzali@gmail.com>
2021-02-17 01:10:42 +0330

    swagger added

--- src/main/java/com/faramarz/spring/Covid19RestApi/Covid19RestApiApplication.java
+++ src/main/java/com/faramarz/spring/Covid19RestApi/Covid19RestApiApplication.java
@@ -3,15 +3,16 @@ package com.faramarz.spring.Covid19RestApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSwagger2
 public class Covid19RestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Covid19RestApiApplication.class, args);
     }
-
+//https://covid19-rest-service.herokuapp.com/api/v1/all/recovered
 }
