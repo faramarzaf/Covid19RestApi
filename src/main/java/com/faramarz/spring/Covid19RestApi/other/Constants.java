3bc90eb4e3fee9145c9dd3f6619edf553a46359d
FaramarzAf <faramarzafzali@gmail.com>
2021-03-14 13:28:54 +0330

    push

--- src/main/java/com/faramarz/spring/Covid19RestApi/other/Constants.java
+++ src/main/java/com/faramarz/spring/Covid19RestApi/other/Constants.java
@@ -3,8 +3,9 @@ package com.faramarz.spring.Covid19RestApi.other;
 
 public class Constants {
 
-    public static final String URL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-    public static final String URL_DEAD = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
-    public static final String URL_RECOVERED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+    private static final String BASE_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/";
+    public static final String URL_CONFIRMED = BASE_URL + "time_series_covid19_confirmed_global.csv";
+    public static final String URL_DEAD = BASE_URL + "time_series_covid19_deaths_global.csv";
+    public static final String URL_RECOVERED = BASE_URL + "time_series_covid19_recovered_global.csv";
 
 }
